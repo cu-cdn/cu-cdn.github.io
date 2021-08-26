63312ca8d0e8f51abd4a7beb36e70175ea986791
Catty2000 <82418813+Catty2000@users.noreply.github.com>
2021-08-27 01:51:58 +0900

    Add files via upload

---
+++ res/shop.js
@@ -0,0 +1,51 @@
+xu.t("shop",function(m,app){
+	m.exports=function(){
+		var content=this,
+		cat=this.querySelector(".cat"),
+		pri=this.querySelectorAll(".price label i"),
+		on={
+			cat:function(){
+				xu.toggle(cat);
+			},
+			ftr:function(a,b){
+				var z=content.querySelector("[name="+a+"].f"+b);
+				z.checked=!z.checked;
+				z.focus();
+			},
+			flr:function(){
+				content.querySelector(".filter [name=filter]").click();
+			},
+			pri_min:function(){
+				self.pri_com(this,pri[0]);
+			},
+			pri_max:function(){
+				self.pri_com(this,pri[1]);
+			}
+		},
+		self={on:on,freg:null,
+			pri_com:function(a,b){
+				b.innerHTML=a.value.replace(/\B(?=(\d{3})+(?!\d))/g,"<b>,</b>");
+			},
+			deploy:function(){
+				var a=content.querySelectorAll(".price label i"),b=content.querySelectorAll(".price label input");
+				this.pri_com(b[0],a[0]);
+				this.pri_com(b[1],a[1]);
+			}
+		};
+
+		try{
+			self.deploy();
+		}catch(e){}
+
+		this.querySelector(".price").onkeyup=function(e){
+			for(var p=e.target,i=16;p&&i>0;p=p.parentNode,i--)
+			switch(p.constructor){
+			case HTMLInputElement:
+				xu.ncall(on,p,e);
+				return;
+			}
+		};
+
+		return self;
+	};
+})
